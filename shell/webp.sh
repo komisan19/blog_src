ffbc069471fee927ff3376e9187ec134c3509a22
komisan19 <18901496+komisan19@users.noreply.github.com>
2021-06-23 19:40:06 +0900

    fix: update webp

--- shell/webp.sh
+++ shell/webp.sh
@@ -3,6 +3,5 @@
 images=$(find ../static/images/ -type f -iname '*'.png)
 for image in $images;
 do
-    base=`basename $image ".png"`
-    cwebp $image -o ../static/images/$base".webp" >/dev/null 2>&1
+    cwebp $image -o $image".webp" >/dev/null 2>&1
 done
